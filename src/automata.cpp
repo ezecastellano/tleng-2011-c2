e9da8847818eaee600f70b75c89df5a759db23fb
marianosemelman@gmail.com <marianosemelman@gmail.com@ea53c4e4-16e8-47e8-c683-fb5bbe769ae1>
2011-12-15 22:47:28 +0000

    casi andando

--- src/automata.cpp
+++ src/automata.cpp
@@ -1,62 +1,41 @@
-#include "automata.h"
-#include <stdlib.h>
-/* /: lambda
- */
- 
-Transition* new_transition(int code) {
-    Transition* t = (Transition*)malloc(sizeof(Transition));
-    t->code = code;
-    t->next = NULL;
-    t->next_size = 0;
-    return t;
-}
+#include "automata.hpp"
 
-void add_transition(Transition* actual, Transition* t_next) {
-    //Pido un arreglo de tamaño next_size + 1
-    Transition** next = (Transition**)malloc(sizeof(Transition*) * (actual->next_size + 1));
-    for(int i = 0; i < actual->next_size - 1; i++) {
-        next[i] = actual->next[i];
-    }
+//Builds an empty automata
+Automata::Automata() {
     
-    next[actual->next_size] = t_next;
-    free(actual->next);
+}
+//Builds an alfanum automata
+Automata::Automata(char){
     
-    actual->next_size++;
+}
+//determinize the automata
+void Automata::determinize(){
     
 }
 
-Automata* build_lambda_automata() {
-    // ^/$
-    Automata* res = (Automata*)malloc(sizeof(Automata));
-    Transition* t_lambda = new_transition(LAMBDA);
-    Transition* tail = new_transition(END);
-    res->init = t_lambda;
-    res->tail = tail;
+Automata Automata::operator|(Automata & other){
     
-    return init;
+}
+//Concatenate two automata
+Automata Automata::operator+(Automata & other){
+    
+}
+//Apply unary operator
+Automata Automata::apply_op(char){
+    
 }
 
+//Match a string
+bool Automata::match(string){
+    
+}
 
-/** */
-Automata automata_handle_expression(Automata data, Automata ops, Automata rest)
-{
-    }
-
-/** */
-Automata concat_automatas(Automata binary, Automata other)
-{
-    }
-
-Automata build_any_alfanum_automata(){
-    }
-
-Automata build_alfanum_automata(char c){
-    }
-/** Given an NDA returns a DA */
-Automata determinize_automata(Automata a)
-{
-    }
-/** Given a Transition and a token it returns the Transition after following that token */
-Automata follow(Automata a, char* token)
-{
-    }
+Automata::Transition::Transition(int code){
+    
+}
+Automata::Transition::Transition(char c){
+    
+}
+void Automata::Transition::add_next(Transition & t){
+    
+}
